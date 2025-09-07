d5dfb40f0dbed6d897aad7e6020e17729cb4047b
durgesh-work <durgesh.bhalerao42@gmail.com>
2025-09-08 00:21:22 +0530

    adding logger to sotre logs in txt file

--- Kafka-Consumer/src/main/java/com/consumer/service/KafkaMessageListener.java
+++ Kafka-Consumer/src/main/java/com/consumer/service/KafkaMessageListener.java
@@ -11,7 +11,22 @@ public class KafkaMessageListener {
 	Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
 	
 	@KafkaListener(topics = "newTopic2", groupId = "jt-group-1")
-	public void consume(String msg) {
-	logger.info("consumer consumed: "+msg);	
+	public void consume1(String msg) {
+	logger.info("consumer1 consumed: "+msg);	
+	}
+	
+	@KafkaListener(topics = "newTopic2", groupId = "jt-group-1")
+	public void consume2(String msg) {
+	logger.info("consumer2 consumed: "+msg);	
+	}
+	
+	@KafkaListener(topics = "newTopic2", groupId = "jt-group-1")
+	public void consume3(String msg) {
+	logger.info("consumer3 consumed: "+msg);	
+	}
+	
+	@KafkaListener(topics = "newTopic2", groupId = "jt-group-1")
+	public void consume4(String msg) {
+	logger.info("consumer4 consumed: "+msg);	
 	}
 }
